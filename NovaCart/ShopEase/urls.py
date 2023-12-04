100179beac4f2ee73b434eb591a0b80b5b5442cc
shini <shini@pop-os.localdomain>
2023-12-04 12:48:22 +0545

    Login Page and Form Created

--- NovaCart/ShopEase/urls.py
+++ NovaCart/ShopEase/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("shop", views.shop, name="shop"),
-    path("shop/<int:product_id>", views.product, name="product")
+    path("shop/<int:product_id>", views.product, name="product"),
+    path("login", views.login, name="login"),
 ]
--- NovaCart/ShopEase/views.py
+++ NovaCart/ShopEase/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import LoginForm
 
 # Create your views here.
 def index(request):
@@ -14,4 +15,17 @@ def product(request, product_id):
     product = Product.objects.get(pk=product_id)
     return render(request, "ShopEase/products/product_detail.html", {
         "product": product,
+    })
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+    else:
+        form = LoginForm()
+        
+    return render(request, "ShopEase/user/login.html", {
+        'form': form
     })
